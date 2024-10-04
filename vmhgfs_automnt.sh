654da5c05d4438973cf2340a20fb65dfe366ee90
HanBitz <32574445+HanBitz@users.noreply.github.com>
2024-10-04 16:28:51 +0900

    Update vmhgfs_automnt.sh to v1.1
    check vmware shared folder is already enabled(check exist /mnt/hgfs)
    show fstab elements when installation error

--- vmhgfs_automnt.sh
+++ vmhgfs_automnt.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
+#VMWare shared folder auto mount script v1.1
+#https://github.com/HanBitz/vmhgfs_automount
+#for DU System programming
 
-value=`cat /etc/fstab`
-cont_val="vmhgfs-fuse"
-if [[ "$value" =~ "$cont_val" ]]; then
-	echo "VM hgfs 자동 마운트가 이미 켜져있습니다."
+#fstab path
+PATH_FSTAB=/etc/fstab
+#vmware shared folder path
+PATH_MNT=/mnt/hgfs
+#value for find installed when check fstab
+VAL_HGFS="vmhgfs-fuse"
+
+#check already installed
+if [ `cat "$PATH_FSTAB" | grep -c "$VAL_HGFS"` -gt 0 ]; then
+	echo "VM 공유폴더 자동 마운트가 이미 켜져있습니다."
 else
-	echo -e "VM hgfs 자동 마운트 설치중..\n"
-	echo "vmhgfs-fuse   /mnt/hgfs    fuse    defaults,allow_other    0    0" >> /etc/fstab
-	value=`cat /etc/fstab`
-	if [[ "$value" =~ "$cont_val" ]]; then
-		echo "설치 성공"
+	echo "VM hgfs 자동 마운트 설치중.."
+	echo ""
+	#check directory available
+	if [ ! -d "$PATH_MNT" ]; then
+		echo "경고! 공유폴더가 설정되지 않은 것 같습니다"
+		echo "VMWare 공유폴더 설정을 먼저 해주세요.."
 	else
-		echo "설치 실패!"
+		#write linux file volume list to auto mount vmware folder
+		#name: vmhgfs-fuse, mount at: /mnt/hgfs, type: fuse(Filesystem in Userspace), [opt], no backup, no check filesystem at boot
+		echo "vmhgfs-fuse   /mnt/hgfs    fuse    defaults,allow_other    0    0" >> /etc/fstab
+		#check applied correctly
+		if [ `cat "$PATH_FSTAB" | grep -c "$VAL_HGFS"` -gt 0 ]; then
+			echo "설치 완료"
+		else
+			echo "설치 실패! 아래 내용과 함께 교수나 조교에게 문의해주세요.."
+			echo ""
+			echo "=== fstab ==="
+			#show fstab elem without comments
+			cat /etc/fstab | grep "^[^#;]" --color=never
+			echo "============="
+		fi
 	fi
 fi
-echo -e "\n쉘 스크립트를 종료합니다.\n\n"
+echo ""
+echo "설치 스크립트를 종료합니다."
